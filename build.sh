e80ec338eeead2d37d8b9bd9f8c70832385d17d8
notYuriy <umz38@yandex.ru>
2020-04-03 15:36:29 +0300

    moved mm headers to the separate folder + support for recursive include

--- build.sh
+++ build.sh
@@ -6,14 +6,22 @@ export LD="$PREFIX/bin/x86_64-elf-g++"
 mkdir obj
 mkdir isotree
 
+includePaths="eval find include -type d"
+includeString=""
+
+for includePath in $($includePaths)
+do
+    includeString="${includeString} -I${includePath}"
+done
+
 cppPaths="eval find . -type f -name '*.cpp'"
 for cppSource in $($cppPaths)
 do
-    echo "[CPP] $cppSource"
+    echo "[ CC ] $cppSource"
     objectPath="obj/cpp_$(basename "$cppSource" .c).o"
 
-    $CPP -c -o $objectPath $cppSource -Iinclude\
-    -ffreestanding -Wall -Wextra -O2 -mno-sse -mno-sse2 -mno-sse3\
+    $CPP -c -o $objectPath $cppSource $includeString\
+    -ffreestanding -funroll-loops -Wall -Wextra -O2 -mno-sse -mno-sse2 -mno-sse3\
     -mcmodel=large -mno-red-zone -fno-exceptions -fno-rtti\
     -fno-asynchronous-unwind-tables -mno-red-zone -Wno-attributes || exit
 done
@@ -21,12 +29,12 @@ done
 asmPaths="eval find . -type f -name '*.s'"
 for asmSource in $($asmPaths)
 do
-    echo "[AS]  $asmSource"
+    echo "[ AS ] $asmSource"
     objectPath="obj/asm_$(basename "$asmSource" .s).o"
     $ASM -f elf64 $asmSource -o $objectPath -w-number-overflow || exit
 done
 
-echo "[LD]  obj/kernel.bin"
+echo "[ LD ] obj/kernel.bin"
 $LD -n -T ld/linker.ld obj/* -o obj/kernel.bin -ffreestanding -O2 -lgcc -nostdlib
 
 mkdir isotree/boot
@@ -34,8 +42,10 @@ mkdir isotree/boot/grub
 cp grub/grub.cfg isotree/boot/grub/grub.cfg
 cp obj/kernel.bin isotree/boot/kernel.bin
 
-echo "[GRUB] grub.cfg"
+echo "[ BL ] grub/grub.cfg"
 grub-mkrescue isotree -o YaYaOS.iso 2>/dev/null
 
+echo "[ RM ] obj"
 rm -rf obj
+echo "[ RM ] isotree"
 rm -rf isotree
